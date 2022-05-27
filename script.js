691a3a8675f76fca778cbe775d3854f929388719
DPLong <dplong742@gmail.com>
2022-05-26 20:33:45 -0400

    iterations

--- script.js
+++ script.js
@@ -2,74 +2,73 @@ var state = "start";
 
 // query selectors
 var startScreen = document.querySelector("#start");
-var question = document.querySelector("#question");
+var quizScreen = document.querySelector("#question");
 var endScreen = document.querySelector("#end");
 var startButton = document.querySelector("#start button");
-var questionTitle = document.querySelector("#question h2");
-// var questionContainer = document.querySelector("#title");
+var questionTitle = document.querySelector("#title");
+var questionText = document.querySelector("#question-text");
+var answerA = document.querySelector("#answer-a");
+var answerB = document.querySelector("#answer-b");
+var answerC = document.querySelector("#answer-c");
+var timerText = document.querySelector("#timer");
 
 // question setup vars
-var questionArray = ["q1", "q2", "q3", "q4"];
-var currentQuestion = 0;
+var currentQ = 0;
 var score = 0;
- 
+var timer = 60;
 var questions = [
-   {
-    question: "What is a man?",
-       answers: {
-           a: "A miserable pile of secrets!",
-           b: "XY",
-           c: "yer mum"
-       },
-       correctAnswer: "a"
-   },
-   {
-    question: "What is a man?",
-       answers: {
-           a: "A miserable pile of secrets!",
-           b: "XY",
-           c: "yer mum"
-       },
-       correctAnswer: "b"
-   },
-   {
-    question: "What is a man?",
-       answers: {
-           a: "A miserable pile of secrets!",
-           b: "XY",
-           c: "yer mum"
-       }, 
-       correctAnswer: "c"
-   },
+    {
+        question: "What is a man?",
+        answerA: "A miserable pile of secrets!",
+        answerB: "XY",
+        answerC: "yer mum",
+        correctAnswer: "answer-a"
+    },
+    {
+        question: "2 + 2",
+        answerA: "77",
+        answerB: "4",
+        answerC: "1984",
+        correctAnswer: "answer-b"
+    },
+    {
+        question: "8 + 8",
+        answerA: "6",
+        answerB: "1",
+        answerC: "16",
+        correctAnswer: "answer-c"
+    },
 ]
-
-//not sure
-function questionSet() {
-    var questionText = document.createElement("p");
-    questionContainer.innerHTML = "";
-    questionText.textContent();
-    questionText.appendChild(questionPlacehold);
-}
 
 // displays block elements that contain "screens"
 function displayState() {
     if (state === "start") {
         startScreen.style.display = "block";
-        question.style.display = "none";
+        quizScreen.style.display = "none";
         endScreen.style.display = "none";
     }
 
     if (state === "question") {
         startScreen.style.display = "none";
-        question.style.display = "block";
+        quizScreen.style.display = "block";
         endScreen.style.display = "none";
     }
 
     if (state === "end") {
         startScreen.style.display = "none";
-        question.style.display = "none";
+        quizScreen.style.display = "none";
         endScreen.style.display = "block";
     }
+}
+
+//html for the question screens needs to be written to the elements and output
+function questionDisplay() {
+    questionTitle.textContent = "Question " + (currentQ + 1);
+    questionText.textContent = questions[currentQ].question;
+    answerA.textContent = questions[currentQ].answerA;
+    answerB.textContent = questions[currentQ].answerB;
+    answerC.textContent = questions[currentQ].answerC;
+    // timerText.textContent = "Remaining time: " + timer;    
 }
 
 // init
@@ -81,9 +80,84 @@ function init() {
 startButton.addEventListener("click", function () {
     state = "question";
     displayState();
+    // timer = 60;
+    // startTimer();
+    questionDisplay();
 });
 
-//TODO: a listener for answers that moves from one question to the next
+
+answerA.addEventListener("click", function (event) {
+    var element = event.target;
+    if (element.matches(questions[currentQ].correctAnswer)) {
+        currentQ++;
+        score++;
+        if (currentQ < questions.length) {
+            questionDisplay();
+        } else {
+            state = "end";
+            displayState();
+        }
+    } else {
+        currentQ++;
+        if (currentQ < questions.length) {
+            questionDisplay();
+        } else {
+            state = "end";
+            displayState();
+        }
+    }
+    console.log(questions.length);
+    console.log("score" + score);
+    console.log("question index" + currentQ);
+});
+
+answerB.addEventListener("click", function (event) {
+    var element = event.target;
+    if (element.matches(questions[currentQ].correctAnswer)) {
+        currentQ++;
+        score++;
+        if (currentQ < questions.length) {
+            questionDisplay();
+        } else {
+            state = "end";
+            displayState();
+        }
+    } else {
+        currentQ++;
+        if (currentQ < questions.length) {
+            questionDisplay();
+        } else {
+            state = "end";
+            displayState();
+        }
+    }
+    console.log("score" + score);
+    console.log("question index" + currentQ);
+});
+
+answerC.addEventListener("click", function (event) {
+    var element = event.target;
+    if (element.matches(questions[currentQ].correctAnswer)) {
+        currentQ++;
+        score++;
+        if (currentQ < questions.length) {
+            questionDisplay();
+        } else {
+            state = "end";
+            displayState();
+        }
+    } else {
+        currentQ++;
+        if (currentQ < questions.length) {
+            questionDisplay();
+        } else {
+            state = "end";
+            displayState();
+        }
+    }
+    console.log("score" + score);
+    console.log("question index" + currentQ);
+});
 
 questionTitle.addEventListener("click", function () {
     state = "end";
@@ -95,3 +169,34 @@ init();
 
 
 //TODO: timer stuff
+// start button making timer count snippet
+// function displayMessage(){
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+//   }
+  
+//   function setTime() {
+//     // Sets interval in variable
+//     displayMessage();
+//     var timerInterval = setInterval(function() {
+//       secondsLeft--;
+//       displayMessage();
+  
+//       if(secondsLeft === 0) {
+//         // Stops execution of action at set interval
+//         clearInterval(timerInterval);
+//         // Calls function to create and append image
+//         sendMessage();
+//       }
+  
+//     }, 1000);
+//   }
+  
+//   // Function to create and append colorsplosion image
+//   function sendMessage() {
+//     timeEl.textContent = " ";
+//     var imgEl = document.createElement("img");
+//     imgEl.setAttribute("src", "images/image_1.jpg");
+//     mainEl.appendChild(imgEl);
+  
+//   }
+  
